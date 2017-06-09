f29d6d720cb7d23e90e66cdc59292cf5bd63c366
cardenb <carden@zoox.com>
2017-06-09 11:44:09 -0700

    Added a step to setup to clean unused plugins with vundle. Also added more comments to setup script for clarity.

--- setup.sh
+++ setup.sh
@@ -25,12 +25,18 @@ cd $CWD
 # Install Vundle, a vim plugin manager.
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# Clean unused plugins with vundle.
+# Append ! to PluginClean to auto-approve removal.
+vim +PluginClean! +q
 # Install Vundle plugins.
+# +q keeps vim from opening a vim session to display status of plugin install.
+# +all installs all plugins
 vim +PluginInstall +qall
 
 # Install TPM, a tmux plugin manager.
+# When you open tmux, use `prefix + I` to fetch the plugins and install.
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# Source config files
+# Source config files for them to take effect.
 source ~/.bashrc
 tmux source ~/.tmux.conf
 
